94f1d47c05ae85fa853e3dbfbfdac2c585d24eb9
Prabesh Acharya <praveshach1032@gmail.com>
2025-09-19 20:20:10 +0545

    chairman

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -71,7 +71,7 @@
 
             <!-- Social -->
             <div>
-                <h2 class="text-xl font-bold text-white mb-4">Connect With Us</h2>
+                <h2 class="text-xl font-bold text-white mb-4">Connect With Us </h2>
                 <div class="flex space-x-4 text-2xl">
                     <a href="https://facebook.com/prabesh.ach" class="text-blue-500 hover:text-blue-400"><i class="ri-facebook-circle-fill"></i></a>
                     <a href="https://twitter.com/PrabeshAch33319" class="text-sky-400 hover:text-sky-300"><i class="ri-twitter-fill"></i></a>
